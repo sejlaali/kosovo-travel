574294d4f55db6c6438cb41c483b84e713a97266
Sejla Ali <sejlaali@gmail.com>
2019-07-09 12:22:09 -0400

    updated seed file with two activities

--- kosovo-1/client/src/components/Activities.js
+++ kosovo-1/client/src/components/Activities.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
+import axios from 'axios'
 
 class Activities extends Component {
+    state = {
+        activities: []
+    }
+
+    async componentDidMount() {
+        const res = await axios.get("http://localhost:3000/posts")
+        const activities = res.data
+        this.setState({
+            activities
+        })
+    }
 
     render() {
         return (
             <div>
-                This is Activities
+                This is Things to See and Do!
+                
                 </div>
         )
     }
--- kosovo-1/db/seeds.rb
+++ kosovo-1/db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Post.create(
-        {title: 'The Rugova Canyon', image_url: "https://res.cloudinary.com/dfzjh0dui/image/upload/v1562682969/7889046558_d3b3f6c256_z.jpg", description: "With its length of 25 km (15.5 miles) and depth of up to 1,000m (3,300 feet) Rugova is one of the most impressive canyons in Europe. The waters of Peć Bistrica river have cut their way through the mountains in a breathtaking way. The town of Peja is a good starting point for a hike along the canyon and the surrounding mountains."}
-)
+Post.create([
+        {title: 'The Rugova Canyon', image_url: "https://res.cloudinary.com/dfzjh0dui/image/upload/v1562682969/7889046558_d3b3f6c256_z.jpg", city: "Peć", description: "With its length of 25 km (15.5 miles) and depth of up to 1,000m (3,300 feet) Rugova is one of the most impressive canyons in Europe. The waters of Peć Bistrica river have cut their way through the mountains in a breathtaking way. Rugova Canyon offers great opportunities for hikers, rock-climbers and cave explorers. A 'Via Ferrata', called the 'Iron Trail', has been built. It is the first and the only one in the Balkans and enables hikers to climb rocks."}, 
+        {title: "Dokufest", image_url: "", city: "Prizren", description: "Dokufest is the largest film festival in Kosovo, held in the beginning of August. In addition to the main theater spaces, improvised screening venues are also installed, including atop the Prizen fortress. During the Festival, the town is full of people and various parties go late at night in the bars. Documentary photo exhibitions, debates, master classes and lively music events are also part of the 9-day festival"}
+])
